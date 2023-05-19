3aff0595fe71665e56fae3657d4ef8e1c7b9a4bc
EpicMo <1982742309@qq.com>
2023-05-19 09:48:46 +0800

    navi

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,5 +1,27 @@
 module.exports = {
   title: 'Netife官方文档',
   description: 'Netife官方文档，提供Netife使用、开发等教程',
-  base: "/NetifeDocs/"
+  base: "/NetifeDocs/",
+  lastUpdated: true,
+  themeConfig: {
+    nav: [
+      { text: "首页", link: "/" }, 
+    ],
+    socialLinks: [{ icon: "github", link: "https://github.com/Netife/NetifeDocs" }],
+    footer: {
+      message: "Released under the GPLV3 License.",
+      copyright: "Copyright © 2023",
+    },
+    // 配置从导航栏进去后的侧边栏
+    sidebar: {
+      "/dev/": [
+        {
+          text:'开发',
+          items: [
+            { text: "插件开发", link: "/dev/plugins/" },
+          ],
+        },
+      ],
+    },
+  },
 }
